a553ffb2bbace2daf9c07f2ca2514ac544809980
salvador-lax <salvadorlax@outlook.es>
2023-10-14 11:32:50 +0200

    redefine routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')
-    ->name('index');
-
-Route::view('/jobs', 'jobs')
+Route::view('/', 'jobs')
     ->name('jobs');
 
-Route::view('/about', 'about')
-    ->name('about');
+Route::view('/info', 'info')
+    ->name('info');
+
+Route::view('/contact', 'contact')
+    ->name('contact');
