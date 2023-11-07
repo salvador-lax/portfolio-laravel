8ffff328a57697f4141e2337c9fd28bc00a16930
Salvador Lax <salvadorlax@outlook.es>
2023-11-07 16:43:50 +0100

    make textareas resizeable on input

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,20 @@
 import './bootstrap';
 import 'remixicon/fonts/remixicon.css';
+
+const textareaList = document.querySelectorAll("textarea");
+
+for (const textarea of textareaList) {
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    textarea.style.overflowY = "hidden";
+
+    textarea.addEventListener(
+        "input",
+        (e) => {
+            const target = e.target;
+
+            target.style.height = 0;
+            target.style.height = `${target.scrollHeight}px`;
+        },
+        false
+    );
+}
