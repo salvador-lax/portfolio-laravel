160ca5308241a0e0bc919ba0499b6d347d726fb7
Salvador Lax <salvadorlax@outlook.es>
2023-11-07 10:28:11 +0100

    add styles & change language to english

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @if (Session::has('sent'))
-        <p>{{ Session::get('sent') }}</p>
+        <p class="mb-4 font-bold text-lg">{{ Session::get('sent') }}</p>
     @endif
 
     <form method="POST" action="{{ route('contactRequest') }}" class="flex flex-col gap-4 child:grid">
@@ -9,28 +9,28 @@
         <label>
             Email:
             @error('email')
-                <span class="alert alert-danger">El email no es valido</span>
+                <span class="alert alert-danger">Email is not valid</span>
             @enderror
             <input name="email" type="email" value="{{ old('email') }}" class="rounded-sm text-black">
         </label>
 
         <label>
-            Teléfono:
+            Phone:
             @error('phone')
-                <span class="alert alert-danger">El teléfono no es valido</span>
+                <span class="alert alert-danger">Phone is not valid</span>
             @enderror
             <input name="phone" type="tel" value="{{ old('phone') }}" class="rounded-sm text-black">
         </label>
 
         <label>
-            Indica un mensaje (opcional):
-            <textarea name="message" maxlength="1000" class="rounded-sm text-black">
-                {{ old('message') }}
+            Send message (optional):
+            <textarea name="message" maxlength="1000" class="rounded-sm text-black" va>
+                {{ old('message', null) }}
             </textarea>
         </label>
 
-        <button type="submit" class="bg-white hover:bg-slate-100  text-black w-fit p-1 mx-auto rounded">
-            Enviar
+        <button type="submit" class="bg-white hover:bg-slate-100 text-black w-fit p-1 mx-auto rounded font-bold">
+            Send
         </button>
     </form>
 </x-layout>
